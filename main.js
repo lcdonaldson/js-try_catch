2db9f71ab4c68145b51e8615be6898b192819488
Levi Donaldson <lcdbacanas@gmail.com>
2017-02-15 20:27:13 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var message, x;
+    message = document.getElementById("message");
+    message.setAttribute("class", "err-color")
+    message.innerHTML = "";
+    x = document.getElementById("demo").value;
+    try { 
+        if(x == "")  throw "Input is empty";
+        if(isNaN(x)) throw "Please use numbers only";
+        x = Number(x);
+        if(x < 5)    throw "That number is too low";
+        if(x > 10)   throw "That number is too high";
+    }
+    catch(err) {
+        message.innerHTML = err;
+    }
+}
